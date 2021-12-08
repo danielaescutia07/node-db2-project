8f0021a5fd182665ac62b776aa934b0cd1dc3bc5
danielaescutia07 <danielaescutia@yahoo.com>
2021-12-07 19:37:59 -0600

    schema created

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,6 +1,17 @@
 exports.up = async function (knex) {
   await knex.schema.createTable('cars', table => {
     table.increments('car_id')
+    table.text('vin')
+      .unique()
+      .notNullable()
+    table.text('make')
+      .notNullable()
+    table.text('model')
+      .notNullable()
+    table.decimal('mileage')
+      .notNullable()
+    table.text('title')
+    table.text('transmission')
   })
 };
 
